9ca8b53db9f60609d9aba715d33d89fb55bab3d7
rickhuang <rick.liyue.huang@gmail.com>
2021-10-07 12:21:50 +1100

    apply form on login

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -9,9 +11,40 @@ export class LoginComponent implements OnInit {
 
   hide = true;
 
-  constructor() { }
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    /*this.form = new FormGroup({
+      email: new FormControl('aa@aa.com',
+        Validators.compose([Validators.required, Validators.email])),
+      password: new FormControl('', Validators.required)
+    })*/
+    this.form = this.fb.group({
+      email: ['aa@a.com', Validators.compose([Validators.required, Validators.email, /*this.validate*/])],
+      password: ['', Validators.required]
+    })
+  }
+
+  onSubmit({value, valid}: {value: any, valid: any}, ev: Event) {
+    ev.preventDefault();
+    console.log(JSON.stringify(value));
+    console.log(JSON.stringify(valid));
+  }
+
+
+  validate(c: FormControl): Record<string, any> {
+    if (!c.value) {
+      return {'a': null};
+    }
+    const pattern = /^huang+/;
+    if (pattern.test(c.value)) {
+      return {'b': null};
+    }
+    return {
+      emailNotValid: 'The email must start with huang'
+    }
   }
 
 }
